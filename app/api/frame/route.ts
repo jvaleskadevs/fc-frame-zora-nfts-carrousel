0a53a1a1c2520719e6a3c7e4c371e7f4dc4376d2
jvaleskadevs <vmjune2019@gmail.com>
2024-01-29 17:46:35 +0100

    metadata from alchemy

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -2,15 +2,37 @@ import { NextRequest, NextResponse } from 'next/server';
 
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  const postUrl = "https://fc-frame-zora-nft-carrousel.app/api/frame";
-  const maxSupply = 46;
-  const randomIndex = Math.floor(Math.random() * maxSupply) + 1;
-  const zoraUrl = "https://opensea.io/assets/base/0x45ab4ace5836190fed42800b1c11cb6bdb3b4dc5";
-  const randomNft = zoraUrl + "/" + randomIndex;
+  const postUrl = "https://fc-frame-zora-nft-carrousel.app/api/frame"; 
+  const lfghoContractAddress = "0x45ab4ace5836190fed42800b1c11cb6bdb3b4dc5";
+  
+/*  
+  // get the total supply
+  const options = {method: 'GET', headers: {accept: 'application/json'}};
+  const baseUrl = 'https://base-mainnet.g.alchemy.com/';
+  const endpoint = baseUrl + `nft/v3/${process.env.ALCHEMY_ID || 'docs-demo'}/getContractMetadata`;
+  const params = `?contractAddress=${lfghoContractAddress}`;
+  const response = await fetch(endpoint+params, options);
+  const lfghoMetadata = await response.json();
+*/
+  
+  // randomize the tokenID
+  const maxSupply = 76; // lfghoMetadata?.totalSupply;
+  const randomTokenId = Math.floor(Math.random() * maxSupply) + 1;
+
+  // fetch nft metadata
+  const options = {method: 'GET', headers: {accept: 'application/json'}};
+  const baseUrl = 'https://base-mainnet.g.alchemy.com/';
+  const endpoint = baseUrl + `nft/v3/${process.env.ALCHEMY_ID || 'docs-demo'}/getNFTMetadata`;
+  const params = `?contractAddress=${lfghoContractAddress}&tokenId=${randomTokenId}&refreshCache=false`;
+  const response = await fetch(endpoint+params, options);
+  const metadata = await response.json();
+
+  // build the image url
+  const nftImageUrl = metadata?.image?.cachedUrl;
 
   return new NextResponse(`<!DOCTYPE html><html><head>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content=${randomNft} />
+    <meta property="fc:frame:image" content=${nftImageUrl} />
     <meta property="fc:frame:button:1" content="Randomize nft" />
     <meta property="fc:frame:post_url" content=${postUrl} />
   </head></html>`);
--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,28 @@ import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
 
 
-const maxSupply = 46;
-const randomIndex = Math.floor(Math.random() * maxSupply) + 1;
-const zoraUrl = "https://opensea.io/assets/base/0x45ab4ace5836190fed42800b1c11cb6bdb3b4dc5";
-const randomImage = zoraUrl + "/" + randomIndex;
-const postUrl = "https://fc-frame-zora-nft-carrousel.app/api/frame";
+const postUrl = "https://fc-frame-zora-nft-carrousel.app/api/frame"; 
+const lfghoContractAddress = "0x45ab4ace5836190fed42800b1c11cb6bdb3b4dc5";
+
+// randomize the tokenID
+const maxSupply = 76;
+const randomTokenId = Math.floor(Math.random() * maxSupply) + 1;
+
+// fetch nft metadata
+const options = {method: 'GET', headers: {accept: 'application/json'}};
+const baseUrl = 'https://base-mainnet.g.alchemy.com/';
+const endpoint = baseUrl + `nft/v3/${process.env.ALCHEMY_ID || 'docs-demo'}/getNFTMetadata`;
+const params = `?contractAddress=${lfghoContractAddress}&tokenId=${randomTokenId}&refreshCache=false`;
+const response = await fetch(endpoint+params, options);
+const metadata = await response.json();
+
+// build the image url
+const nftImageUrl = metadata?.image?.cachedUrl;
+
 
 const frameMetadata = getFrameMetadata({
   buttons: ['randomize nft'],
-  image: randomImage,
+  image: nftImageUrl,
   post_url: postUrl,
 });
 
@@ -20,7 +33,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'nft carrousel',
     description: 'nft carrousel',
-    images: [randomImage],
+    images: [nftImageUrl],
   },
   other: {
     ...frameMetadata,
@@ -30,7 +43,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
-      <h1>this is a farcaster frame displaying an nft carrousel</h1>
+      <h1>this is a farcaster frame displaying an nfts carrousel</h1>
     </>
   );
 }
