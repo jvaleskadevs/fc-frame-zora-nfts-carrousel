ae513368919e64ae481a062fcbca00dd8b41bdb8
jvaleskadevs <vmjune2019@gmail.com>
2024-01-29 19:28:17 +0100

    fixFrameApiResponse

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -2,24 +2,12 @@ import { NextRequest, NextResponse } from 'next/server';
 
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-/*  
-  // get the total supply
-  const options = {method: 'GET', headers: {accept: 'application/json'}};
-  const baseUrl = 'https://base-mainnet.g.alchemy.com/';
-  const endpoint = baseUrl + `nft/v3/${process.env.ALCHEMY_ID || 'docs-demo'}/getContractMetadata`;
-  const params = `?contractAddress=${lfghoContractAddress}`;
-  const response = await fetch(endpoint+params, options);
-  const lfghoMetadata = await response.json();
-*/
-  
   const postUrl = "https://fc-frame-zora-nft-carrousel.app/api/frame"; 
   const lfghoContractAddress = "0x45ab4ace5836190fed42800b1c11cb6bdb3b4dc5";
 
-  // randomize the tokenID
   const maxSupply = 76;
   const randomTokenId = Math.floor(Math.random() * maxSupply) + 1;
 
-  // fetch nft metadata
   const options = {method: 'GET', headers: {accept: 'application/json'}};
   const baseUrl = 'https://base-mainnet.g.alchemy.com/';
   const endpoint = baseUrl + `nft/v3/${process.env.ALCHEMY_ID || 'docs-demo'}/getNFTMetadata`;
@@ -32,13 +20,17 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
 
   return new NextResponse(`<!DOCTYPE html><html><head>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content=${nftImageUrl} />
+    <meta property="fc:frame:image" content="${nftImageUrl}" />
     <meta property="fc:frame:button:1" content="Randomize nft" />
-    <meta property="fc:frame:post_url" content=${postUrl} />
+    <meta property="fc:frame:post_url" content="${postUrl}" />
   </head></html>`);
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
+  return getResponse(req);
+}
+
+export async function GET(req: NextRequest): Promise<Response> {
   return getResponse(req);
 }
 
